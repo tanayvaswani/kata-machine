15787ad5b3e130af4d911c9097da973e3b0d07d3
tanayvaswani <vaswani.tanay9@gmail.com>
2024-03-02 13:26:23 +0530

    chore: Binary Search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let l = 0;
+    let h = haystack.length;
 
+    do {
+        const m = Math.floor(l + (h - l) / 2);
+        const v = haystack[m];
+
+        if (v === needle) {
+            return true;
+        } else if (v > needle) {
+            h = m;
+        } else {
+            l = m + 1;
+        }
+    } while (l < h);
+
+    return false;
 }
